497501023a90c129d5cab27be249da98143c4b08
anil reddy chinala <anilreddychinala@gmail.com>
2025-01-26 11:24:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.toggle-btn').addEventListener('click', function() {
+    const flashlight = document.querySelector('.flashlight');
+    flashlight.classList.toggle('dark-mode'); // Toggle dark mode class for light effect
+});
